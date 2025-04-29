c251703a64dd9dee1ef93c1b2265cb92326096ec
elemyalcinkaya <22290419@ogrenci.ankara.edu.tr>
2025-04-29 12:26:03 +0300

    backend ve frontend iskeleti olusturuldu

---
+++ backend/backend/src/test/java/com/SmartEnergyFlow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartEnergyFlow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
